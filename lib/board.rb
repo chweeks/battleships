f61aa9a7fd70aa8d4faa4368b1906d8380a9b967
chn.challenger <chn.challenger@gmail.com>
2015-09-07 11:54:31 +0100

    First test for place_ship

--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,14 @@ class Board
   attr_accessor :grid
 
   def initialize
-    @grid = [[],[]]
+    @grid = [[0,0,0,0],
+    				 [0,0,0,0],
+    				 [0,0,0,0],
+    				 [0,0,0,0]]
   end
 
   def place(ship, location)
+		grid[location[0]][location[1]] = ship   	
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,13 +5,15 @@ describe Board do
   it { is_expected.to respond_to :place }
 
   it 'Board is a grid' do
-    expect(subject.instance_variable_get(:@grid).class).to eql Array
+    #expect(subject.instance_variable_get(:@grid).class).to eql Array
+    expect(subject.grid.is_a?(Array)).to eq(true)
   end
 
   describe '#place'do
     it 'places ship in correct grid position' do
-      ship = :ship
-      subject.place(ship, location)
+      ship = double(:ship)
+      subject.place(ship, [0,0])
+      expect(subject.grid[0][0]).to eq(ship)
     end
   end
 
