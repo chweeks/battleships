8e4d546ea995fdb9b52d38b6b98874c3ed1ba9ad
chweeks <chrisharryweeks@gmail.com>
2015-09-07 16:27:12 +0100

    added fire method

--- lib/board.rb
+++ lib/board.rb
@@ -4,32 +4,18 @@ class Board
 
   def initialize
     @grid = []
-    10.times{@grid << [0,0,0,0,0,0,0,0,0,0]}
+    10.times{@grid << ['O','O','O','O','O','O','O','O','O','O']}
   end
 
   def show
   	grid.each{|row| p row}
-  end
-
-  def outside?(x,y)
-  	x < 0 || y < 0 || x >= grid.size || y >= grid[0].size
-  end
-
-  def overlap?(x,y)
-  	grid[x][y] != ''
-  end
-
-  def array_xy(x,y)
-    xy = []
-    xy << x
-    xy << y
-    xy
+    nil
   end
 
   def place(ship, location, orientation)
     x, y = location[0], location[1]
     co_ords = []
-    ship.size.times do
+    ship.times do
       fail 'outside' if outside?(x,y)
       fail 'overlap' if overlap?(x,y)
       case orientation
@@ -48,9 +34,31 @@ class Board
       end
       co_ords << xy
     end
-    co_ords.each{|co_ord| grid[co_ord[0]][co_ord[1]] = ship}
+    co_ords.each{|co_ord| grid[co_ord[0]][co_ord[1]] = 'S'}
   end
+
+  def fire(x,y)
+    if grid[x][y] == 'O'
+      grid[x][y] = 'M'
+    elsif grid[x][y] == 'S'
+      grid[x][y] == 'X'
+    end
+  end
+
+  private
+
+    def outside?(x,y)
+    	x < 0 || y < 0 || x >= grid.size || y >= grid[0].size
+    end
+
+    def overlap?(x,y)
+    	grid[x][y] != 'O'
+    end
+
+    def array_xy(x,y)
+      xy = []
+      xy << x
+      xy << y
+      xy
+    end
 end
-
-
-
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,10 @@ describe Board do
 
   it { is_expected.to respond_to(:place).with(3).arguments }
 
+  it { is_expected.to respond_to(:show) }
+
+  it { is_expected.to respond_to(:fire) }
+
   it 'Board is a grid' do
     expect(subject.grid.is_a?(Array)).to eq(true)
   end
@@ -11,49 +15,68 @@ describe Board do
   describe '#place'do
 
     it 'places ship in correct grid position when orientation north' do
-      ship = double :ship, size: 2
+      ship = 2
       subject.place(ship, [2,0], "north")
-      expect(subject.grid[2][0]).to eq(ship)
-      expect(subject.grid[1][0]).to eq(ship)
+      expect(subject.grid[2][0]).to eq('S')
+      expect(subject.grid[1][0]).to eq('S')
     end
 
     it 'places ship in correct grid position when orientation south' do
-      ship = double :ship, size: 2
+      ship = 2
       subject.place(ship, [0,0], "south")
-      expect(subject.grid[0][0]).to eq(ship)
-      expect(subject.grid[1][0]).to eq(ship)
+      expect(subject.grid[0][0]).to eq('S')
+      expect(subject.grid[1][0]).to eq('S')
     end
 
     it 'places ship in correct grid position when orientation east' do
-      ship = double :ship, size: 2
+      ship = 2
       subject.place(ship, [0,0], "east")
-      expect(subject.grid[0][0]).to eq(ship)
-      expect(subject.grid[0][1]).to eq(ship)
+      expect(subject.grid[0][0]).to eq('S')
+      expect(subject.grid[0][1]).to eq('S')
     end
 
     it 'places ship in correct grid position when orientation west' do
-      ship = double :ship, size: 2
+      ship = 2
       subject.place(ship, [0,1], "west")
-      expect(subject.grid[0][1]).to eq(ship)
-      expect(subject.grid[0][0]).to eq(ship)
+      expect(subject.grid[0][1]).to eq('S')
+      expect(subject.grid[0][0]).to eq('S')
     end
 
     it 'raise error if asked to place ship out of the board' do
-      ship = double :ship, size: 5
+      ship = 11
       expect{subject.place(ship, [0,0], "east")}.to raise_error "outside"
     end
 
     it 'ship not place when error raised' do
-      ship = double :ship, size: 5
+      ship = 11
       expect{subject.place(ship, [0,0], "east")}.to raise_error "outside"
       expect(subject.grid.include? ship).to be false
     end
 
     it 'raise error when trying to place a ship overlapping another ship' do
-      ship1 = double :ship, size: 3
-      ship2 = double :ship, size: 3
+      ship1 = 3
+      ship2 = 3
       subject.place(ship1,[0,0],"south")
       expect{subject.place(ship2,[1,0],"east")}.to raise_error "overlap"
+    end
+  end
+
+  describe '#fire' do
+    context "When it's a hit" do
+      it 'change cell to X' do
+        ship = 2
+        subject.place(ship, [0,0], "east")
+        subject.fire(0,0)
+        expect(subject.grid[0][0]).to eql 'X'
+      end
+    end
+    context "When it's a miss" do
+      it 'change cell to M' do
+        ship = 2
+        subject.place(ship, [1,0], "east")
+        subject.fire(0,0)
+        expect(subject.grid[0,0]).to eql 'M'
+      end
     end
   end
 end
