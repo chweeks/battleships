2e96479399aec6526ff13780f1e2dbbc97dc8afa
chn.challenger <chn.challenger@gmail.com>
2015-09-07 13:27:16 +0100

    worked on placing ship method and test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,11 +9,32 @@ describe Board do
   end
 
   describe '#place'do
+
     it 'places ship in correct grid position when orientation north' do
-      ship = double(:ship, size: 3)
+      ship = double(:ship, size: 2)
       subject.place(ship, [2,0], "north")
       expect(subject.grid[2][0]).to eq(ship)
       expect(subject.grid[1][0]).to eq(ship)
+    end
+
+    it 'places ship in correct grid position when orientation south' do
+      ship = double(:ship, size: 2)
+      subject.place(ship, [0,0], "south")
+      expect(subject.grid[0][0]).to eq(ship)
+      expect(subject.grid[1][0]).to eq(ship)
+    end
+
+    it 'places ship in correct grid position when orientation east' do
+      ship = double(:ship, size: 2)
+      subject.place(ship, [0,0], "east")
+      expect(subject.grid[0][0]).to eq(ship)
+      expect(subject.grid[0][1]).to eq(ship)
+    end
+
+    it 'places ship in correct grid position when orientation west' do
+      ship = double(:ship, size: 2)
+      subject.place(ship, [0,1], "west")
+      expect(subject.grid[0][1]).to eq(ship)
       expect(subject.grid[0][0]).to eq(ship)
     end
   end
